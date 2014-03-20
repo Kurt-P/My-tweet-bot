8bf3b9c38d28acf67d626e7d1d6a80a29d03097b
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-03-20 18:04:19 -0500

    Improved tweeter
    Passed in new args into `fortune`. The first one `-n 140` will
    only return fortunes that are 140 chars in length. This remoed
    the need to check the length of the `line` var. The seconde
    being `-s` which only give fortuens that are "short" or 160
    chars or less. I though that this might be a little redudnent
    with the `-n 140` arg, except when fortuens with more than 200
    chars were being passed into the `line` var.
    
    Added logging function. Info about OAuth and HTTP status are
    logged as well as the line that was tweeted and the lengthe
    of the line.

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ It uses BSD Fortune to tweet a random fortune to my time line.
 
 It was a fun afternoon project.
 
+###Setup
+Run `crontab -e` as a normal user and add the following line:
+`0 * * * * /home/$USER/path/to/tweeter.py`
+
+Note: I do not know if useing the `$USER` variable will work, so to be safe
+you may want to use your username or home dir name instead.
+
 ###Resources
-[MakeUseOf](http://www.makeuseof.com/tag/how-to-build-a-raspberry-pi-twitter-bot/)
-[BSD Fortune](http://www.bsdfortune.com)
+* [MakeUseOf](http://www.makeuseof.com/tag/how-to-build-a-raspberry-pi-twitter-bot/)
+* [BSD Fortune](http://www.bsdfortune.com)
+* [Twython](http://twython.readthedocs.org/en/latest/)
+* [Google](https://google.com)
--- tweeter.py
+++ tweeter.py
@@ -3,6 +3,7 @@
 from sys import argv
 from twython import Twython
 import commands
+import logging
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -11,13 +12,18 @@ ACCESS_SECRET = ''
 
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-cmd = '/usr/games/fortune -a'
+# Do some lgging
+logging.basicConfig(filename='tweeter.log', level=logging.DEBUG)
+
+# See 'man fortune' for more info
+cmd = '/usr/games/fortune -a -n 140 -s'
 
 def quote():
     line = commands.getoutput(cmd)
-    if len(line) > 140:
-        quote()
-    else:
-        api.update_status(status = line)
+#    print line
+    api.update_status(status = line)
+    logging.debug('line %s twetted', line)
+    logging.info('line len: %i', len(line))
 
+# Do it!
 quote()
