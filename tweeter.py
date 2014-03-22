66815046cde6e32b55980ee60f35aa84492f5728
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-03-22 00:22:29 -0500

    Update tweeter.py
    Took logging stuff out, b/c when run by cron, logging did nothing. May come back to it later in the future.

--- tweeter.py
+++ tweeter.py
@@ -3,7 +3,6 @@
 from sys import argv
 from twython import Twython
 import commands
-import logging
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -12,18 +11,13 @@ ACCESS_SECRET = ''
 
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-# Do some lgging
-logging.basicConfig(filename='tweeter.log', level=logging.DEBUG)
-
 # See 'man fortune' for more info
 cmd = '/usr/games/fortune -a -n 140 -s'
 
 def quote():
     line = commands.getoutput(cmd)
-#    print line
+#    print line # Used for testing
     api.update_status(status = line)
-    logging.debug('line %s twetted', line)
-    logging.info('line len: %i', len(line))
 
 # Do it!
 quote()
