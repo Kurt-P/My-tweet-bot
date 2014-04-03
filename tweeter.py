510ce4a14af5e11be319f97db03e76bc936d2be9
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-04-03 16:37:10 -0500

    Added logging back into tweeter
    After I realized that the log file was being placed into my $HOME
    dir, I decided to bring logging back to the script. Also renamed
    the LICENSE file to UNLICENSE.md (B/c I like markdown and I'm
    using the UNLICENSE)

--- tweeter.py
+++ tweeter.py
@@ -3,21 +3,27 @@
 from sys import argv
 from twython import Twython
 import commands
+import logging
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_KEY = ''
 ACCESS_SECRET = ''
 
+# Setup the api object
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+
+# Logging DEBUG
+logging.basicConfig(filename='tweeter.log', level=logging.DEBUG)
 
 # See 'man fortune' for more info
 cmd = '/usr/games/fortune -a -n 140 -s'
 
 def quote():
     line = commands.getoutput(cmd)
-#    print line # Used for testing
-    api.update_status(status = line)
+#    print line # Used for testing; uncomment for testing
+    api.update_status(status = line) # Comment out for testing
+    logging.info("Tweeted: %r", line) # Log the line that was tweeted.
 
 # Do it!
 quote()
