fe06029a012efb638867a135ad05fe4298e1441d
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-09-19 03:02:53 -0400

    Added timestamps to the logging.
    Made the logging output to file a little easier to read by adding date/timestamps to the logging.

--- tweeter.py
+++ tweeter.py
@@ -14,7 +14,8 @@ ACCESS_TOKEN_SECRET = ''
 api = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Logging DEBUG
-logging.basicConfig(filename='tweeter.log', level=logging.DEBUG)
+logging.basicConfig(filename='tweeter.log', level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 
 # See 'man fortune' for more info
 cmd = '/usr/games/fortune -a -n 140 -s'
