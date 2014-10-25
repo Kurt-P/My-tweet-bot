a2b572406a5d78553429dde668c45f644c5455ae
Kurt R. Prudhomme <prud0042@crk.umn.edu>
2014-10-25 19:51:27 -0400

    Moved logging methods above api call

--- tweeter.py
+++ tweeter.py
@@ -21,10 +21,10 @@ logging.basicConfig(filename='tweeter.log', level=logging.DEBUG,
 cmd = 'fortune -a -n 140 -s'
 
 def tweet(_string, length):
-#    print _string # User for testing; uncomment for testing
-    api.update_status(status=_string) # comment out for testing
     logging.info("Tweeted: %r", _string) # Log the line that was tweeted
     logging.info("Tweet length: %d", length) # Log the tweet length
+    api.update_status(status=_string) # comment out for testing
+#    print _string # Uncomment for testing
 
 
 ## NOTE ##
