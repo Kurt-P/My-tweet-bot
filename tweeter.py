e15e6c9ba93f9479152a27cc4d33386c1531dba7
Kurt Prudhomme <krprudhomme@gmail.com>
2015-09-25 17:26:23 -0400

    Code clean up
    * Removed old code from origional version.
    * Changed logging time stamp to match ISO 8061
        (Total time stamp does not meet standard; will address later)
    * Tested scrip

--- tweeter.py
+++ tweeter.py
@@ -15,7 +15,7 @@ api = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Logging DEBUG
 logging.basicConfig(filename='tweeter.log', level=logging.INFO,
-    format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        format='%(asctime)s:%(levelname)s:%(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
 # See 'man fortune' for more info
 cmd = 'fortune -n 140 -s'
@@ -24,23 +24,13 @@ def tweet(_string, length):
     logging.info("Tweeted: %r", _string) # Log the line that was tweeted
     logging.info("Tweet length: %d", length) # Log the tweet length
     api.update_status(status=_string) # comment out for testing
-#    print _string # Uncomment for testing
-
-
-## NOTE ##
-## Leave this sectino for now, I will delete it later;
-## After more testing has been done.
-#def quote():
-#    line = commands.getoutput(cmd)
-#    print line # Used for testing; uncomment for testing
-#    api.update_status(status = line) # Comment out for testing
-#    logging.info("Tweeted: %r", line) # Log the line that was tweeted.
+#   print (_string) # Uncomment for testing
 
 def quote():
     line = subprocess.getoutput(cmd)
     line_length = len(line)
 
-    if line_length >= 140:
+    if line_length >= 141: # Anything under 140 is valid
         quote()
     else:
         tweet(line, line_length)
